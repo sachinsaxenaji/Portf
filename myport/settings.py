23009b83364126765cee854b75606a2d99aba04a
sachinsaxenaji <39828137+sachinsaxenaji@users.noreply.github.com>
2020-04-25 21:45:45 +0530

    Update settings.py

--- myport/settings.py
+++ myport/settings.py
@@ -129,4 +129,4 @@ os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'medias')
